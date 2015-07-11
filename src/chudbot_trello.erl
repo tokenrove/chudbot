34d7036a188531ca50185c02aeb670be49a68184
Julian Squires <julian@cipht.net>
2015-07-11 17:06:08 -0400

    Add some convenience functions for working with Trello
    To be organized into a more useful form later.

--- src/chudbot_trello.erl
+++ src/chudbot_trello.erl
@@ -1,5 +1,32 @@
 -module(chudbot_trello).
--export([handle_request/2]).
+-export([handle_request/2, start/0, list_boards/1, subscribe_webhook/3]).
+
+%%% Trello API stuff
+
+start() ->
+    application:ensure_all_started(ssl),
+    inets:start().
+
+list_boards(Config) ->
+    Key = proplists:get_value(key, Config),
+    Token = proplists:get_value(token, Config),
+    Uri = lists:flatten(["https://trello.com/1/members/my/boards?key=", Key, "&token=", Token]),
+    {ok, {{_, 200, _}, _, Body}} = httpc:request(Uri),
+    lists:map(fun (M) -> {maps:get(<<"name">>, M), maps:get(<<"id">>,M)} end,
+              jiffy:decode(Body, [return_maps])).
+
+subscribe_webhook(Model, Config, BaseUrl) ->
+    Key = proplists:get_value(key, Config),
+    Token = proplists:get_value(token, Config),
+    ContentType = "application/json",
+    Uri = lists:flatten(["https://trello.com/1/tokens/", Token, "/webhooks/?key=", Key]),
+    Body = jiffy:encode(#{description => "Chudbot watching boards",
+                          'callbackURL' => (BaseUrl ++ "/chudbot/trello"),
+                          'idModel' => Model}),
+    {ok, {{_, Status, _}, _, _}} = httpc:request(post, [Uri, [], ContentType, Body], [], []),
+    Status.
+
+%%% Dealing with notifications
 
 handle_request(Body, _Req) ->
     Json = jiffy:decode(Body, [return_maps]),
@@ -10,41 +37,28 @@ handle_request(Body, _Req) ->
                    pp_who_dunnit(Action),
                    pp_action(Action)]).
 
-subscribe_webhook(Model, Key, Token, BaseUrl) ->
-    ContentType = "application/json",
-    Uri = lists:flatten(["https://trello.com/1/tokens/", Token, "/webhooks/?key=", Key]),
-    Body = jiffy:encode(#{description => "Chudbot watching boards",
-                          'callbackURL' => (BaseUrl ++ "/chudbot/trello"),
-                          'idModel' => Model}),
-    {ok, {{_, Status, _}, _, _}} = httpc:request(post, [Uri, [], ContentType, Body], [], []),
-    Status.
-
 pp_who_dunnit(Action) ->
     maps:get(<<"fullName">>, maps:get(<<"memberCreator">>, Action)).
 
 pp_action(Action) ->
-    pp_action(maps:get(<<"type">>, Action), maps:get(<<"data">>, Action)).
-
-pp_action(<<"updateCheckItemStateOnCard">>, Data) ->
+    Data = maps:get(<<"data">>, Action),
     Card = maps:get(<<"name">>, maps:get(<<"card">>, Data)),
+    pp_action(maps:get(<<"type">>, Action), Data, Card).
+
+pp_action(<<"updateCheckItemStateOnCard">>, Data, Card) ->
     Item = maps:get(<<"checkItem">>, Data),
     Name = maps:get(<<"name">>, Item),
     Verb = [maps:get(<<"state">>, Item), "d"],
     [Verb, " ", $\", Name, $\", " on ", $\", Card, $\"];
-pp_action(<<"addMemberToCard">>, Data) ->
-    Card = maps:get(<<"name">>, maps:get(<<"card">>, Data)),
+pp_action(<<"addMemberToCard">>, Data, Card) ->
     Name = maps:get(<<"idMember">>, Data),
     ["added ",Name," to ",$\",Card,$\"];
-pp_action(<<"removeMemberFromCard">>, Data) ->
-    Card = maps:get(<<"name">>, maps:get(<<"card">>, Data)),
+pp_action(<<"removeMemberFromCard">>, Data, Card) ->
     Name = maps:get(<<"idMember">>, Data),
     ["removed ",Name," from ",$\",Card,$\"];
-pp_action(<<"createCard">>, Data) ->
-    Card = maps:get(<<"name">>, maps:get(<<"card">>, Data)),
+pp_action(<<"createCard">>, _Data, Card) ->
     ["created ",$\",Card,$\"];
-pp_action(<<"updateCard">>, Data) ->
-    Card = maps:get(<<"name">>, maps:get(<<"card">>, Data)),
+pp_action(<<"updateCard">>, _Data, Card) ->
     ["updated ",$\",Card,$\"];
-pp_action(S, Data) ->
-    Card = maps:get(<<"name">>, maps:get(<<"card">>, Data)),
+pp_action(S, _Data, Card) ->
     [S, "'d", " on ", $\",Card,$\"].
